95606a58fa0498a3a94cc9542cc7db124e74a9b8
Lance Zotigh <lancezotighlz@gmail.com>
2020-02-19 12:08:26 -0700

    Add project files.

---
+++ FunctionsDemo/FunctionsDemo.cpp
@@ -0,0 +1,23 @@
+// FunctionsDemo.cpp
+// Lance Zotigh (lzotigh1@cnm.edu)
+// Date: 2/19/2020
+// Pourpose Demo Functions
+
+#include <iostream>
+
+using namespace std;
+
+void Header(); //prototype
+
+int main() //definition
+{
+	Header();
+
+	return 0;
+}
+
+void Header()
+{
+	cout << "Welcome to Functions Demo!" << endl;
+}
+
