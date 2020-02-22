a326418d34c5e58afd81dc5fa593e042b51d976d
Lance Zotigh <lancezotighlz@gmail.com>
2020-02-21 20:17:05 -0700

    Adding more functions

--- FunctionsDemo/FunctionsDemo.cpp
+++ FunctionsDemo/FunctionsDemo.cpp
@@ -9,9 +9,11 @@
 using namespace std;
 // prototypes
 void Header();
-string ToUpperCase(string text);
 void Write(string text);
 string GetTextFromUser();
+string convertTextToLower(string text);
+string convertTextToUpper(string text);
+string getTextFromUser();
 
 int main()
 {
@@ -20,10 +22,41 @@ int main()
 	userText = GetTextFromUser();
 	DisplayText(userText);//argument
 
-	string uc = ToUpperCase(userText);
+	textToConvert = getTextFromUser();
+
+	string uc = convertTextToUpper(userText);
 	cout << uc << endl;
 
+	//show text in lower case 
+	lowerCaseText = convertTextToLower(textToConvert);
+	cout << "Lower case version" << lowerCaseText << endl;
+
+	//ask user to input some text to search for
+	cout << endl << "Late's fid something in your text." << endl;
+	textToFind = getTextFromUser();
+
+	//find and report the position of that text
+	int textLocation = textToConvert.find(textToFind);
+	if (textLocation != string::npos)
+	{
+		cout << "Found " << textToFind << " in " << textToConvert
+			<< " at " << textLocation << "!" << endl;
+
+	} else 
+	{
+		cout << "Text not found!" << endl;
+	}
+
 	return 0;
+}
+
+//Get user input
+string getTextFromUseer() 
+{
+	string lineOfText;
+	cout << "Please enter a line of text: ";
+		getline(cin, lineOfText);
+	return lineOfText;
 }
 
 // Definitions
@@ -50,14 +83,32 @@ void DisplayText(string text)//parameter
 	cout << text << endl;
 }
 
-string ToUpperCase(string text) 
+string convertTextToUpper(string text)
 {
 	string uppercase = "";
 
-	for (int i = 0; i < text.length(); ++i) 
+	for (int i = 0; i < text.length(); ++i)
 	{
 		//uppercase to lowercase
 		uppercase += toupper(text.at(i));
 	}
 	return uppercase;
 }
+
+	// Convert to lower case
+	string convertTextToLower(string text) 
+	{
+		string resultText = "";
+
+		for (int i = 0; i < text.size(); ++i) 
+		{
+			resultText += tolower(text[i]);
+		}
+		return resultText;
+	}
+
+}
+
+
+
+
